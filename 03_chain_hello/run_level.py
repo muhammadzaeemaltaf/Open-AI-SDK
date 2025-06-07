5c46f447973f2f8f6dc128f5465eb603672f295c
muhammadzaeemaltaf <official.zaeemaltaf@gmail.com>
2025-06-07 22:11:51 +0500

    remove api key

--- 03_chain_hello/run_level.py
+++ 03_chain_hello/run_level.py
@@ -1,7 +1,7 @@
 from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
 from agents.run import RunConfig
 
-gemini_api_key = "AIzaSyAPjJtievHhVgEZioHVAXJtYVdk1WatlHM"
+gemini_api_key = "API KEY"
 
 external_client = AsyncOpenAI(
     api_key=gemini_api_key,
